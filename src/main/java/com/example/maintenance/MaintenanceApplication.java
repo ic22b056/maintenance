8b669e27c6921860dfedbd5a885d4815fdeb947f
Kenneth Manheim <Kenneth.Manheim@gmail.com>
2023-01-04 20:07:21 +0100

    setmsg create

--- src/main/java/com/example/maintenance/MaintenanceApplication.java
+++ src/main/java/com/example/maintenance/MaintenanceApplication.java
@@ -3,6 +3,8 @@ package com.example.maintenance;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// Du bist im »setmsg branch«! (Falls dus vergisst)
+
 @SpringBootApplication
 public class MaintenanceApplication {
 
